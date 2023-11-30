60c9ae7c01822bdbde36797891f386e8d1caedb3
Chaitanya <chaitanya.aggarwal.07@gmail.com>
2023-11-30 12:14:51 +0530

    Automatic update

--- Autorgressive_Adaptation/data/Opp_HAR/preprocess.py
+++ Autorgressive_Adaptation/data/Opp_HAR/preprocess.py
@@ -5,6 +5,8 @@ import torch.nn.functional as fun
 import argparse
 import pandas as pd
 
+DEBUG = 1
+
 class TimeSeriesDataset(Dataset):
     def __init__(self, file_paths, window_size, overlap):
         self.data = [self.load_data(file_path) for file_path in file_paths]
@@ -21,28 +23,35 @@ class TimeSeriesDataset(Dataset):
     def load_data(self, file_path):
         # Load data from the .dat file using np.loadtxt
         data = np.loadtxt(file_path)
-        return data
+        content = data[:,:243]
+        label = data[:,243]
+        return (content, label)
 
     def clean_data(self):
         datas = []
-        for data in self.data:
-            df = pd.DataFrame(data)
+        for content, label in self.data:
+            df = pd.DataFrame(content)
             df.interpolate('linear', inplace=True)
-            datas.append(df.to_numpy())
+            df.dropna(axis=1,inplace=True)
+            nulls = df.isna().to_numpy()
+            zeros = np.zeros(nulls.shape)
+            zeros[nulls] = 1
+            print(list(np.sum(zeros,axis=0)))
+            datas.append((df.to_numpy(), label))
         self.data = datas
             
     def get_label(self, data, idx):
         check_map = {0:0,1:1,2:2,4:3,5:3}
-        return torch.tensor(check_map[data[idx+self.window_size-1:idx + self.window_size, 243].item()], dtype=torch.int64)
+        return torch.tensor(check_map[data[idx+self.window_size-1:idx + self.window_size].item()], dtype=torch.int64)
 
     def process_data(self):
         self.clean_data()
         processed_content = []
         processed_labels = []
-        for data in self.data:
-            for idx in range(0, len(data) - self.window_size + 1, int(self.window_size * (1 - self.overlap))):
-                content_data = torch.tensor(data[idx:idx + self.window_size, :113], dtype=torch.float32)
-                labels_data = self.get_label(data,idx)
+        for (data_con, data_label) in self.data:
+            for idx in range(0, len(data_con) - self.window_size + 1, int(self.window_size * (1 - self.overlap))):
+                content_data = torch.tensor(data_con[idx:idx + self.window_size, :113], dtype=torch.float32)
+                labels_data = self.get_label(data_label,idx)
                 content_tensor = torch.tensor(content_data, dtype=torch.float32)
                 labels_tensor = torch.tensor(labels_data, dtype=torch.float32)
                 processed_content.append(content_tensor)
@@ -73,10 +82,18 @@ def main():
             output_file_path = sec+'_'+name+'.pt'
 
             #Creat the datasets
-            dataset = TimeSeriesDataset(input_file_paths, args.window_size, args.overlap)
+            dataset = TimeSeriesDataset(input_file_paths, args.window_size, args.overlap)            
 
+            print(dataset.content)
+
+            print(dataset.content.any().item())
+
+            if (DEBUG):
+                break
             # Save the processed data as a .pt file
             torch.save({'samples':dataset.content, 'labels':dataset.labels}, output_file_path)
+        if (DEBUG):
+            break
 
 if __name__ == "__main__":
     main()
